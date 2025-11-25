52f9180daf54adea2538d8c26ba9bc216115a960
yusuftuncay <yusuf@tuncay.be>
2025-11-25 22:13:50 +0100

    feat: update CORS allowed origins

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,7 @@ const DEFAULT_ALLOWED_ORIGINS = ["http://localhost:4000", "*"];
 
 const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
   ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
-  : ["http://127.0.0.1:5500", "https://letswatch.one", "*"];
+  : ["http://127.0.0.1:5500", "http://127.0.0.1:3000", "https://letswatch.one", "*"];
 
 export const corsConfig = cors({
     allowMethods: ["GET"],
